73b4a5a695035396c0de8bd5b36327499a9e2c5e
陈杨子 <yangzi.cyz@alibaba-inc.com>
2021-10-18 20:05:58 +0800

    add 19.remove-nth-node-from-end-of-list.js(2pointer-1)

---
+++ leetcode/19.remove-nth-node-from-end-of-list.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=19 lang=javascript
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (36.68%)
+ * Likes:    7208
+ * Dislikes: 369
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given the head of a linked list, remove the n^th node from the end of the
+ * list and return its head.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1], n = 1
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * 
+ * 
+ * 
+ * Follow up: Could you do this in one pass?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  if (head.next === null) return null;
+  let steps = 0;
+  let slow = head;
+  let fast = head;
+  while (fast !== null) {
+    if (steps > n) {
+      slow = slow.next;
+    }
+    steps = steps + 1;
+    fast = fast.next;
+  }
+  if (steps === n) {
+    return head.next;
+  }
+  slow.next = slow.next.next;
+
+  return head;
+};
+// @lc code=end
